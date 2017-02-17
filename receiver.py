c8eee6c3e9646b87570a4ceaf5e488d81169ff27
Philip Lundrigan <philiplundrigan@gmail.com>
2017-02-17 06:42:46 +0000

    Add decryption and validation to receiver

--- receiver.py
+++ receiver.py
@@ -2,6 +2,8 @@ import argparse
 
 import pyshark
 
+import utils
+
 FILTER = "wlan[4] == 0x33 and wlan[5] == 0x33 and wlan[16] == 0xfe"
 
 
@@ -13,7 +15,9 @@ def process_packet(packet):
     dst_data = dst.split(':')[2:]
 
     data = ''.join(src_data + dst_data)
-    data = bytearray.fromhex(data).decode()
+    data = bytearray.fromhex(data)
+
+    sequence = int(sequence, base=16)
 
     return sequence, data
 
@@ -24,10 +28,37 @@ def get_wifi_information(interface):
                                   monitor_mode=True,
                                   capture_filter=FILTER)
     capture.set_debug()
-    capture.apply_on_packets(process_packet)
 
+    packets = {}
     for packet in capture.sniff_continuously():
-        data = process_packet(packet)
+        sequence, data = process_packet(packet)
+
+        if sequence not in packets:
+            print("Received", sequence)
+            packets[sequence] = data
+
+        if len(packets) == 4:
+            break
+
+    print(packets)
+
+    # Pull out the data and pull out the MAC
+    packets = sorted(packets.items())
+
+    encrypted_data = b''.join([value for key, value in packets[:-2]])
+    hash_data = b''.join([value for key, value in packets[-2:]])
+
+    print(encrypted_data)
+    print(hash_data)
+
+    if hash_data != utils.hash_message(key=b'key', message=encrypted_data):
+        print("MAC is different. The data is invalid")
+        # TODO: Try again
+        return
+
+    data = utils.decrypt_message(key=b'1234567894123456',
+                                 message=encrypted_data)
+    print(data)
 
 
 if __name__ == '__main__':
